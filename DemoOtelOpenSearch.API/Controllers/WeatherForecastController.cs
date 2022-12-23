2e6a9a0f8b5d9469f19da49112904ca0e2237287
Martin Paquin <martin.paquin@gmail.com>
2022-12-22 21:01:17 -0500

    Folder structure

--- DemoOtelOpenSearch.API/Controllers/WeatherForecastController.cs
+++ DemoOtelOpenSearch.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Sentry;
 
 namespace DemoOtelOpenSearch.API.Controllers;
 
@@ -12,21 +14,50 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger,
+        HttpClient httpClient,
+        IConfiguration configuration)
     {
         _logger = logger;
+        _httpClient = httpClient;
+        _configuration = configuration;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet("hello")]
+    public string SayHello()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        return "Hello World";
+    }
+    
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public async Task<IEnumerable<WeatherForecast>> Get()
+    {
+        using var scope = _logger.BeginScope("{Id}", 
+            Guid.NewGuid().ToString("N"));
+
+        var sayHelloUrl = _configuration.GetValue<string>("SayHelloAPI");
+        var result = await _httpClient
+            .GetStringAsync(sayHelloUrl);
+        
+        _logger.LogInformation("Say Hello Result : {result}", result);
+        
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+        _logger.LogError(
+            "WeatherForecasts generated {count}: {forecasts}",
+            forecasts.Length,
+            forecasts);
+
+        return forecasts;
     }
 }
--- DemoOtelOpenSearch.API/Program.cs
+++ DemoOtelOpenSearch.API/Program.cs
@@ -1,6 +1,80 @@
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using DemoOtelOpenSearch.API.Controllers;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHttpClient<WeatherForecastController>();
+
+
+builder.Logging.ClearProviders();
+
+// Define some important constants to initialize tracing with
+const string serviceName = "DemoOtelOpenSearch.API";
+const string serviceVersion = "1.0.0";
+var activitySource = new ActivitySource(serviceName);
+
+var appResourceBuilder = ResourceBuilder.CreateDefault()
+    .AddService(serviceName: serviceName, serviceVersion: serviceVersion);
+
+builder.Logging.AddOpenTelemetry(openTelemetryLoggerOptions =>
+{
+    openTelemetryLoggerOptions.IncludeScopes = true;
+    openTelemetryLoggerOptions.IncludeFormattedMessage = true;
+    openTelemetryLoggerOptions.ParseStateValues = true;
+    
+    openTelemetryLoggerOptions
+        .AddConsoleExporter()
+        .AddOtlpExporter(opt =>
+        {
+            var endpoint = builder.Configuration.GetValue<string>("OpenTelemetry:gRPCEndpoint");
+            opt.Endpoint = new Uri(endpoint!);
+            opt.Protocol = OtlpExportProtocol.Grpc;
+        })
+        .SetResourceBuilder(appResourceBuilder);
+});
+
+// Configure to send data via the OTLP exporter.
+// By default, it will send to port 4318, which the collector is listening on.
+builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
+{
+    tracerProviderBuilder
+        .AddConsoleExporter()
+        .AddOtlpExporter(opt =>
+        {
+            var endpoint = builder.Configuration.GetValue<string>("OpenTelemetry:gRPCEndpoint");
+            opt.Endpoint = new Uri(endpoint!);
+            opt.Protocol = OtlpExportProtocol.Grpc;
+        })
+        .AddSource(activitySource.Name)
+        .SetResourceBuilder(appResourceBuilder)
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddSqlClientInstrumentation();
+});
+
+var meter = new Meter(serviceName);
+builder.Services.AddOpenTelemetryMetrics(metricProviderBuilder =>
+{
+    metricProviderBuilder
+        //.AddConsoleExporter()
+        .AddOtlpExporter(opt =>
+        {
+            var endpoint = builder.Configuration.GetValue<string>("OpenTelemetry:gRPCEndpoint");
+            opt.Endpoint = new Uri(endpoint!);
+            opt.Protocol = OtlpExportProtocol.Grpc;
+        })
+        .AddMeter(meter.Name)
+        .SetResourceBuilder(appResourceBuilder)
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation();
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
